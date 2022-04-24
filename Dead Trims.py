c543a5381ddb532a5e11a757908cd9a13cac9312
Bat-Sam <54631899+Bat-Sam@users.noreply.github.com>
2022-04-25 02:18:34 +0600

    Add files via upload

---
+++ Dead Trims.py
@@ -0,0 +1,23 @@
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
+total_price = 0
+for add in prices:
+  total_price += add 
+average_price = total_price / len(prices)
+print("Average Haircut Price:",average_price)
+new_prices = [num-5 for num in prices]
+print("New Prices:",new_prices)
+total_revenue = 0
+for i in range(len(hairstyles)):
+  total_revenue += prices[i] * last_week[i]
+print("Total Revenue:",total_revenue)
+average_daily_revenue = total_revenue / 7
+print("Average Daily Revenue:", average_daily_revenue)
+
+cuts_under_30 = [hairstyles[i] for i in range(len(hairstyles)) if new_prices[i] < 30 ]
+print("Hairstyles which cost less than 30$:", cuts_under_30)
+
+
